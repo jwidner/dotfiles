aa498dd3336343b25e2bdf626ebe2c35799beac0
Jesse Widner <jesse@widner.xyz>
2023-09-08 16:12:07 -0700

    Fix setup.sh and make useful

--- setup.sh
+++ setup.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 set -euo pipefail
-SCRIPT_DIR=""  # TODO
+SCRIPT_DIR="$HOME/dotfiles"
 
 # Vim setup
-# TODO: 0. setup the dirs
-# TODO: 1. move the config files
+mkdir -p \
+    "$HOME/.vim/swap" \
+    "$HOME/.vim/undo" \
+    "$HOME/.vim/backup"
+find "$HOME/.vim" -type d -exec chmod 700 '{}' \;
 cp "$SCRIPT_DIR/.vimrc" "$HOME/.vimrc"
-# TODO: 2. clone the plugins
+# plugins
+PLUGIN_DIR="$HOME/.vim/pack"
+# vim-commentary
+mkdir -p "$PLUGIN_DIR/tpope/start"
+git clone https://tpope.io/vim/commentary.git "$PLUGIN_DIR/tpope/start/commentary"
+vim -u NONE -c "helptags $PLUGIN_DIR/tpope/start/commentary/doc" -c q
+# vim-fugitive
+git clone https://tpope.io/vim/fugitive.git "$PLUGIN_DIR/tpope/start/fugitive"
+vim -u NONE -c "helptags $PLUGIN_DIR/tpope/start/fugitive/doc" -c q
 
-# TODO: Neovim setup
-
-# TODO: tmux setup
+# tmux setup
 cp "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
 TMUX_DIR="$HOME/.tmux"
 mkdir -p "$TMUX_DIR/tmux-resurrect"
 git clone https://github.com/tmux-plugins/tmux-resurrect "$TMUX_DIR/tmux-resurrect"
 
-# TODO: bash setup
-
+# bash setup
+cp "$SCRIPT_DIR/.bashrc" "$HOME/.bashrc"
 . "$HOME/.bashrc"
 
 # Git setup
+cp "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
 GLOBAL_GIT_CONFIG="$HOME/.config/git"
 mkdir -p "$GLOBAL_GIT_CONFIG"
 # TODO: add global git ignore file and use instead of below
