b505f65d01f4920463af4b448b747fa8bae9d408
Jesse Widner <jesse@widner.xyz>
2023-10-31 16:41:23 -0700

    Cleanup setup.sh
    - Use POSIX sh instead of bash
    - Redirect INFO and ERROR use of `echo` to stderr
    - Use braces around shell variables
    - Update git repositories (vim & tmux plugins) if they exist

--- setup.sh
+++ setup.sh
@@ -1,79 +1,91 @@
-#!/bin/bash
-set -euo pipefail
-SCRIPT_DIR="$HOME/dotfiles"
+#!/bin/sh
+set -eu
+SCRIPT_DIR="${HOME}/dotfiles"
 
 # Vim setup
 mkdir -p \
-    "$HOME/.vim/swap" \
-    "$HOME/.vim/undo" \
-    "$HOME/.vim/backup"
-find "$HOME/.vim" -type d -exec chmod 700 '{}' \;
-cp "$SCRIPT_DIR/.vimrc" "$HOME/.vimrc"
+    "${HOME}/.vim/swap" \
+    "${HOME}/.vim/undo" \
+    "${HOME}/.vim/backup"
+find "${HOME}/.vim" -type d -exec chmod 700 '{}' \;
+cp "${SCRIPT_DIR}/.vimrc" "${HOME}/.vimrc"
 
 # vim plugins
-PLUGIN_DIR="$HOME/.vim/pack"
+PLUGIN_DIR="${HOME}/.vim/pack"
 
 # vim-commentary
-mkdir -p "$PLUGIN_DIR/tpope/start"
-if [ ! -e "$PLUGIN_DIR/tpope/start/commentary" ]; then
-    git clone https://tpope.io/vim/commentary.git "$PLUGIN_DIR/tpope/start/commentary"
+mkdir -p "${PLUGIN_DIR}/tpope/start"
+if [ ! -e "${PLUGIN_DIR}/tpope/start/commentary" ]; then
+    git clone 'https://tpope.io/vim/commentary.git' "${PLUGIN_DIR}/tpope/start/commentary"
 else
-    echo "INFO: skipping vim-commentary because it already exists..."
+    echo "INFO: updating existing vim-commentary..." >&2
+    cd "${PLUGIN_DIR}/tpope/start/commentary"
+    git pull --ff-only
 fi
-vim -u NONE -c "helptags $PLUGIN_DIR/tpope/start/commentary/doc" -c q
+vim -u NONE -c "helptags ${PLUGIN_DIR}/tpope/start/commentary/doc" -c q
 
 # vim-fugitive
-mkdir -p "$PLUGIN_DIR/tpope/start"
-if [ ! -e "$PLUGIN_DIR/tpope/start/fugitive" ]; then
-    git clone https://tpope.io/vim/fugitive.git "$PLUGIN_DIR/tpope/start/fugitive"
+mkdir -p "${PLUGIN_DIR}/tpope/start"
+if [ ! -e "${PLUGIN_DIR}/tpope/start/fugitive" ]; then
+    git clone 'https://tpope.io/vim/fugitive.git' "${PLUGIN_DIR}/tpope/start/fugitive"
 else
-    echo "INFO: skipping vim-fugitive because it already exists..."
+    echo "INFO: updating existing vim-fugitive..." >&2
+    cd "${PLUGIN_DIR}/tpope/start/fugitive"
+    git pull --ff-only
 fi
-vim -u NONE -c "helptags $PLUGIN_DIR/tpope/start/fugitive/doc" -c q
+vim -u NONE -c "helptags ${PLUGIN_DIR}/tpope/start/fugitive/doc" -c q
 
 # vim-fzf
-mkdir -p "$PLUGIN_DIR/packages/start"
-if [ ! -e "$PLUGIN_DIR/packages/start/fzf" ]; then
-    git clone https://github.com/junegunn/fzf.git "$PLUGIN_DIR/packages/start/fzf"
+mkdir -p "${PLUGIN_DIR}/packages/start"
+if [ ! -e "${PLUGIN_DIR}/packages/start/fzf" ]; then
+    git clone 'https://github.com/junegunn/fzf.git' "${PLUGIN_DIR}/packages/start/fzf"
 else
-    echo "INFO: skipping fzf because it already exists..."
+    echo "INFO: updating existing fzf..." >&2
+    cd "${PLUGIN_DIR}/packages/start/fzf"
+    git pull --ff-only
 fi
-if [ ! -e "$PLUGIN_DIR/packages/start/fzf.vim" ]; then
-    git clone https://github.com/junegunn/fzf.vim.git "$PLUGIN_DIR/packages/start/fzf.vim"
+if [ ! -e "${PLUGIN_DIR}/packages/start/fzf.vim" ]; then
+    git clone 'https://github.com/junegunn/fzf.vim.git' "${PLUGIN_DIR}/packages/start/fzf.vim"
 else
-    echo "INFO: skipping fzf.vim because it already exists..."
+    echo "INFO: updating existing fzf.vim..." >&2
+    cd "${PLUGIN_DIR}/packages/start/fzf.vim"
+    git pull --ff-only
 fi
 
 # gruvbox
-mkdir -p "$PLUGIN_DIR/default/start"
-if [ ! -e "$PLUGIN_DIR/default/start/gruvbox" ]; then
-    git clone https://github.com/morhetz/gruvbox.git "$PLUGIN_DIR/default/start/gruvbox"
+mkdir -p "${PLUGIN_DIR}/default/start"
+if [ ! -e "${PLUGIN_DIR}/default/start/gruvbox" ]; then
+    git clone https://github.com/morhetz/gruvbox.git "${PLUGIN_DIR}/default/start/gruvbox"
 else
-    echo "INFO: skipping gruvbox because it already exists..."
+    echo "INFO: updating existing gruvbox..." >&2
+    cd "${PLUGIN_DIR}/default/start/gruvbox"
+    git pull --ff-only
 fi
 
 # tmux setup
-cp "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
-TMUX_DIR="$HOME/.tmux"
-mkdir -p "$TMUX_DIR"
-if [ ! -e "$TMUX_DIR/tmux-resurrect" ]; then
-    git clone https://github.com/tmux-plugins/tmux-resurrect "$TMUX_DIR/tmux-resurrect"
+cp "${SCRIPT_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
+TMUX_DIR="${HOME}/.tmux"
+mkdir -p "${TMUX_DIR}"
+if [ ! -e "${TMUX_DIR}/tmux-resurrect" ]; then
+    git clone https://github.com/tmux-plugins/tmux-resurrect "${TMUX_DIR}/tmux-resurrect"
 else
-    echo "INFO: skipping tmux-resurrect because it already exists..."
+    echo "INFO: updating existing tmux-resurrect..." >&2
+    cd "${TMUX_DIR}/tmux-resurrect"
+    git pull --ff-only
 fi
 
 # bash setup
-cp "$SCRIPT_DIR/.bashrc" "$HOME/.bashrc"
-cp "$SCRIPT_DIR/.bash_profile" "$HOME/.bash_profile"
-. "$HOME/.bashrc"
+cp "${SCRIPT_DIR}/.bashrc" "${HOME}/.bashrc"
+cp "${SCRIPT_DIR}/.bash_profile" "${HOME}/.bash_profile"
+. "${HOME}/.bashrc"
 
 # Git setup
-cp "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
-GLOBAL_GIT_CONFIG="$HOME/.config/git"
-mkdir -p "$GLOBAL_GIT_CONFIG"
+cp "${SCRIPT_DIR}/.gitconfig" "${HOME}/.gitconfig"
+GLOBAL_GIT_CONFIG="${HOME}/.config/git"
+mkdir -p "${GLOBAL_GIT_CONFIG}"
 # TODO: add global git ignore file and use instead of below
-echo '/tags' >"$GLOBAL_GIT_CONFIG/ignore"
+echo '/tags' >"${GLOBAL_GIT_CONFIG}/ignore"
 
 # local scripts
-mkdir -p "$HOME/.local/bin"
-cp "$SCRIPT_DIR/.local/bin"/* "$HOME/.local/bin"
+mkdir -p "${HOME}/.local/bin"
+cp "${SCRIPT_DIR}/.local/bin"/* "${HOME}/.local/bin"
